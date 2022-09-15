a5311f4211ec1b35ba732a6c5d3910bf28d68da3
dankelleher <dankelleher@yahoo.com>
2022-09-15 20:08:46 +0200

    Cleaned up, simplified and updated react component interface.

--- src/Gateway.js
+++ src/Gateway.js
@@ -7,50 +7,39 @@ import { useWallet } from '@solana/wallet-adapter-react';
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const env = {
-  prod: {
     gatekeeperNetwork: new PublicKey('ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6'),
     clusterUrl: 'https://api.mainnet-beta.solana.com',
     cluster: 'mainnet-beta',
-  },
-  test: {
-    gatekeeperNetwork: new PublicKey('tigoYhp9SpCDoCQmXGj2im5xa3mnjR1zuXrpCJ5ZRmi'),
-    clusterUrl: 'https://api.devnet.solana.com',
-    cluster: 'devnet',
-    stage: 'preprod',
-  }
 };
 
 function RequestGatewayToken() {
-  const { gatewayStatus,requestGatewayToken, gatewayToken } = useGateway();
-  return (
-      <>
-          <div>Wallet adapter connected</div>
-          <div>Pass status: {GatewayStatus[gatewayStatus]}</div>
-          <br />
-          <button type='submit' onClick={requestGatewayToken}>Request Pass</button>
-          <br />
-          <div>Pass: {gatewayToken?.publicKey.toBase58()}</div>
-      </>
-  )
+    const { gatewayStatus,requestGatewayToken, gatewayToken } = useGateway();
+    return (
+        <>
+            <div>Wallet adapter connected</div>
+            <div>Pass status: {GatewayStatus[gatewayStatus]}</div>
+            <br />
+            <button type='submit' onClick={requestGatewayToken}>Request Pass</button>
+            <br />
+            <div>Pass: {gatewayToken?.publicKey.toBase58()}</div>
+        </>
+    )
 }
 
 function Gateway() {
-  const wallet = useWallet();
-  const { publicKey } = wallet;
-  const { gatekeeperNetwork, stage, cluster, clusterUrl } = env.prod;
-  console.log(`publicKey:${publicKey}`);
-  return (
-    /* gatekeeperSendsTransaction disabled is the default behavior and the flag can be omitted */
-    <GatewayProvider
-      wallet={wallet}
-      gatekeeperNetwork={gatekeeperNetwork}
-      stage={stage}
-      cluster={cluster}
-      clusterUrl={clusterUrl}
-      gatekeeperSendsTransaction={false}>
-        { publicKey && <RequestGatewayToken /> }
-    </GatewayProvider>
-  )
+    const wallet = useWallet();
+    const { publicKey } = wallet;
+    const { gatekeeperNetwork, cluster, clusterUrl } = env;
+    console.log(`publicKey:${publicKey}`);
+    return (
+        <GatewayProvider
+            wallet={wallet}
+            gatekeeperNetwork={gatekeeperNetwork}
+            cluster={cluster}
+            clusterUrl={clusterUrl}>
+            { publicKey && <RequestGatewayToken /> }
+        </GatewayProvider>
+    )
 }
 
 export default Gateway;
--- src/IdentityButton.js
+++ src/IdentityButton.js
@@ -2,14 +2,7 @@ import { Keypair, PublicKey } from '@solana/web3.js';
 import { GatewayProvider, useGateway, IdentityButton, GatewayStatus } from '@civic/solana-gateway-react';
 
 const env = {
-  prod: {
-    gatekeeperNetwork: new PublicKey('ni1jXzPTq1yTqo67tUmVgnp22b1qGAAZCtPmHtskqYG'),
-  },
-  test: {
-    gatekeeperNetwork: new PublicKey('tniC2HX5yg2yDjMQEcUo1bHa44x9YdZVSqyKox21SDz'),
-    clusterUrl: 'https://api.devnet.solana.com',
-    stage: 'preprod',
-  }
+  gatekeeperNetwork: new PublicKey('ni1jXzPTq1yTqo67tUmVgnp22b1qGAAZCtPmHtskqYG'),
 };
 
 const userKeypair = Keypair.generate();
@@ -25,25 +18,24 @@ const dummyWallet = {
 function IdentityComponent() {
   const { gatewayToken, gatewayStatus } = useGateway();
   return (
-    <>
-      <IdentityButton />
-      <br/>
-      <small>Gateway status: <b>{GatewayStatus[gatewayStatus]}</b></small>
-      {gatewayToken && <small>Gateway token: <b>{gatewayToken?.publicKey.toBase58()}</b></small>}
-    </>
+      <>
+        <IdentityButton />
+        <br/>
+        <small>Gateway status: <b>{GatewayStatus[gatewayStatus]}</b></small>
+        {gatewayToken && <small>Gateway token: <b>{gatewayToken?.publicKey.toBase58()}</b></small>}
+      </>
   )
 };
 
 function CivicIdentityButton() {
-  const { gatekeeperNetwork, stage, clusterUrl } = env.prod;
+  const { gatekeeperNetwork, stage, clusterUrl } = env;
   return (
-    <GatewayProvider 
-      wallet={dummyWallet} 
-      gatekeeperNetwork={gatekeeperNetwork}
-      stage={stage}
-      clusterUrl={clusterUrl}>
-      <IdentityComponent/>
-    </GatewayProvider>
+      <GatewayProvider
+          wallet={dummyWallet}
+          gatekeeperNetwork={gatekeeperNetwork}
+          clusterUrl={clusterUrl}>
+        <IdentityComponent/>
+      </GatewayProvider>
   )
 };
 
